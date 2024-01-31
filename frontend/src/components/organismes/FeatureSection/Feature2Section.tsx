291745f749de3f6663883de3f0f54492726fac9e
Gabriel Goldbronn <74196777+Gabouil@users.noreply.github.com>
2024-01-31 23:36:34 +0100

    init : Rajout de la deuxième section feature

---
+++ frontend/src/components/organismes/FeatureSection/Feature2Section.tsx
@@ -0,0 +1,43 @@
+import "./FeatureSection.scss";
+import Banner from "../../../assets/images/FeatureBanner2.png"
+import Button from "../../atomes/buttons/Button/Button.tsx";
+
+export default function Feature2Section() {
+    return (
+        <>
+            <section className="feature_section feature_section--reverse">
+                <h2 className="feature_section__title">
+                    Découvrez la fraîcheur de nos ingrédients
+                </h2>
+                <div className="feature_section__banner">
+                    <img src={Banner} alt="Banner"/>
+                </div>
+                <div className="feature_section__content--list">
+                    <div className="feature_section__content__item">
+                        <h3 className="feature_section__content__item__title">
+                            Ingrédients frais
+                        </h3>
+                        <p className="feature_section__content__item__text">
+                            On sait depuis longtemps que travailler avec du texte lisible et contenant du sens est source de distractions, et empêche de se concentrer sur la mise en page elle-même.
+                        </p>
+                    </div>
+                    <div className="feature_section__content__item">
+                        <h3 className="feature_section__content__item__title">
+                            Repas délicieux et sains
+                        </h3>
+                        <p className="feature_section__content__item__text">
+                            Contrairement à une opinion répandue, le Lorem Ipsum n'est pas simplement du texte aléatoire. Il trouve ses racines dans une oeuvre de la littérature latine classique datant de 45 av. J.-C., le rendant vieux de 2000 ans.
+                        </p>
+                    </div>
+                </div>
+                <div className="feature_section__button">
+                    <Button
+                        label="Voir les recettes"
+                        type="NavLink"
+                        link="/recipes"
+                    />
+                </div>
+            </section>
+        </>
+    )
+}
--- frontend/src/components/templates/Home/Home.tsx
+++ frontend/src/components/templates/Home/Home.tsx
@@ -1,12 +1,14 @@
 import "./Home.scss"
 import HeroHeaderSection from "../../organismes/HeroHeaderSection/HeroHeaderSection.tsx";
 import FeatureSection from "../../organismes/FeatureSection/FeatureSection.tsx";
+import Feature2Section from "../../organismes/FeatureSection/Feature2Section.tsx";
 
 export default function Home() {
     return (
         <>
             <HeroHeaderSection/>
             <FeatureSection/>
+            <Feature2Section/>
         </>
     )
 }

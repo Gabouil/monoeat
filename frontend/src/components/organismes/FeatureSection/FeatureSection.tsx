cd6d1ffcb9e7bf4cfff1a6d985cc37ab16d79bcf
Gabriel Goldbronn <74196777+Gabouil@users.noreply.github.com>
2024-01-31 23:20:42 +0100

    init: création de la section des features

---
+++ frontend/src/components/organismes/FeatureSection/FeatureSection.tsx
@@ -0,0 +1,63 @@
+import "./FeatureSection.scss";
+import Banner from "../../../assets/images/FeatureBanner1.png"
+import Button from "../../atomes/buttons/Button/Button.tsx";
+
+export default function FeatureSection() {
+    return (
+        <>
+            <section className="feature_section">
+                <h2 className="feature_section__title">
+                    Des recettes faciles à suivre pour des repas savoureux à la maison
+                </h2>
+                <div className="feature_section__banner">
+                    <img src={Banner} alt="Banner"/>
+                </div>
+                <div className="feature_section__content">
+                    <div className="feature_section__content__item">
+                        <h3 className="feature_section__content__item__title">
+                            Préparation rapide
+                        </h3>
+                        <p className="feature_section__content__item__text">
+                            Gagnez du temps en cuisine grâce à nos kits de repas rapides et pratiques.
+                        </p>
+                    </div>
+                    <div className="feature_section__content__item">
+                        <h3 className="feature_section__content__item__title">
+                            Variété d'options
+                        </h3>
+                        <p className="feature_section__content__item__text">
+                            Choisissez parmi un large éventail de recettes pour répondre à vos goûts et à vos
+                            préférences alimentaires.
+                        </p>
+                    </div>
+                    <div className="feature_section__content__item">
+                        <h3 className="feature_section__content__item__title">
+                            Qualité
+                        </h3>
+                        <p className="feature_section__content__item__text">
+                            Nous sélectionnons soigneusement chaque ingrédient pour nous assurer qu'il répond à nos
+                            normes de qualité strictes.
+                        </p>
+                    </div>
+                    <div className="feature_section__content__item">
+                        <h3 className="feature_section__content__item__title">
+                            Commodité
+                        </h3>
+                        <p className="feature_section__content__item__text">
+                            Avec MonoEat, vous pouvez profiter de la commodité d'avoir des ingrédients frais livrés
+                            à
+                            votre porte.
+                        </p>
+                    </div>
+                </div>
+                <div className="feature_section__button">
+                    <Button
+                        label="Voir les recettes"
+                        type="NavLink"
+                        link="/recipes"
+                    />
+                </div>
+            </section>
+        </>
+    )
+}
--- frontend/src/components/templates/Home/Home.tsx
+++ frontend/src/components/templates/Home/Home.tsx
@@ -1,10 +1,12 @@
 import "./Home.scss"
 import HeroHeaderSection from "../../organismes/HeroHeaderSection/HeroHeaderSection.tsx";
+import FeatureSection from "../../organismes/FeatureSection/FeatureSection.tsx";
 
 export default function Home() {
     return (
         <>
             <HeroHeaderSection/>
+            <FeatureSection/>
         </>
     )
 }

938139ef7e2d042bd0ce5bef78a7feb416531185
Gabriel Goldbronn <74196777+Gabouil@users.noreply.github.com>
2024-04-15 14:42:14 +0200

    fix : lien api

--- frontend/src/services/axios/axiosInstance.tsx
+++ frontend/src/services/axios/axiosInstance.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseURL = 'https://api.monoeat.gabrielgoldbronn.com/'
+const baseURL = 'http://gabrielgoldbronn.com:3000/'
 
 export const AxiosInstance = axios.create({
     baseURL: baseURL,

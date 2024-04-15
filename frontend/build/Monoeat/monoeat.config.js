046d033d25fcee8656aa2fa4af8d3a3cfed37d2e
Gabriel Goldbronn <74196777+Gabouil@users.noreply.github.com>
2024-04-15 12:23:20 +0200

    fix : build

--- frontend/build/Monoeat/monoeat.config.js
+++ frontend/build/Monoeat/monoeat.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [{
         name: "Monoeat",
-        script: "/home/debian/www/Monoeat",
+        script: "/home/debian/www/Monoeat/source/build/Monoeat/index.js",
         env: {
             NODE_ENV: "development",
         },
@@ -15,5 +15,18 @@ module.exports = {
         env_production: {
             NODE_ENV: "production",
         }
-    }]
+    }, ],
+
+    deploy: {
+        production: {
+            user: "debian",
+            host: "141.94.22.241",
+            ref: "origin/main",
+            repo: "git@github.com:Gabouil/monoeat.git",
+            path: "/home/debian/www/Monoeat",
+            'pre-deploy-local': '',
+            "post-deploy": "npm install && npm run build && cd build/Portfolio && npm install && pm2 startOrRestart ecosystem.config.js --env production",
+            'pre-setup': ''
+        }
+    }
 }

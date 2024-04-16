fadc5cd43e7dd5b5d704b34fa76441771c6a1ded
Gabriel Goldbronn <74196777+Gabouil@users.noreply.github.com>
2024-04-16 20:05:55 +0200

    fix : call api

--- frontend/src/services/axios/axiosInstance.tsx
+++ frontend/src/services/axios/axiosInstance.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
-// const baseURL = 'https://api.monoeat.gabrielgoldbronn.com/'
-const baseURL = 'http://localhost:3000/'
+const baseURL = 'https://api.monoeat.gabrielgoldbronn.com/'
+// const baseURL = 'http://localhost:3000/'
 
 export const AxiosInstance = axios.create({
     baseURL: baseURL,
